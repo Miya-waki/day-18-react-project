ba954c67a36d9f4045bc9a1c3ffba5407ed273a6
Miya-waki <rahulgiri12@gmail.com>
2022-01-03 20:44:37 +0530

    reactbootstrap

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
- import "./App.css";
-
-/**
- *style => ""
-style = "background-color:red"
-
- *rule
- style ={{}} 
-style ={{backgroundColor:"red"}}
-
-first two letter css properties are now converted into camelCase property
-the value of the property must be in double quote
- */
 
 export default function App() {
   return (
-    <div>
-      <div style ={{color:"white", backgroundColor:"royalblue"}}>hello world</div>
+    <div className ="container-fluid">
+      <div className ="row" >
+<div className="col">react</div>
+      </div>
     </div>
   );
 }
